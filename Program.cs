84dbd6160b183cbc4c9362ba4cd3eff994fd4b2a
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-24 19:35:10 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Solicite ao usuário que digite a sua senha (uma string). Exiba "Acesso permitido" caso a senha digitada seja 1234abcd, senão exiba "Acesso negado".
+Console.WriteLine("--- Senha ---");
+
+Console.Write("Digite a senha.....:");
+string senha = Console.ReadLine()!;
+
+if (senha == "1234abcd"){
+    Console.WriteLine("Acesso permetido");
+}else  {
+    Console.WriteLine("Acesso negado");
+}
+
+
